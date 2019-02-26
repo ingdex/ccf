f611632d5599d6ae670cf94f33a0a5e9817f04dc
ingdex <871958943@qq.com>
2019-02-26 15:16:00 +0800

    201803-3

--- 201803-3.cpp
+++ 201803-3.cpp
@@ -22,12 +22,46 @@ int match(string rule, string name, string url)
     int rpos1, rpos2, upos1, upos2;
     string str1, str2;
     string re;
-    rpos1 = rule.find('/');
-    upos1 = url.find('/');
-    rpos2 = rule.find('/', rpos1+1);
-    upos2 = url.find('/', upos1+1);
-    str1 = rule.substr(rpos1+1, rpos2-rpos1-1);
-    str2 = url.substr(upos1+1, upos2-upos1-1);
+    rpos1 = rpos2 = upos1 = upos2 = 0;
+    //get the first str to match
+    if (str1[rpos1] == '/')
+    {
+        if (str2[upos1] != '/')
+        {
+            return 0;
+        }
+        rpos1 = upos1 = 1;
+        rpos2 = rule.find('/', rpos1+1);
+        upos2 = url.find('/', upos1+1);
+        str1 = rule.substr(rpos1+1, rpos2-rpos1-1);
+        str2 = url.substr(upos1+1, upos2-upos1-1);
+    }
+    else
+    {
+        if (str2[upos1] == '/')
+        {
+            return 0;
+        }
+        rpos2 = rule.find('/', rpos1+1);
+        upos2 = url.find('/', upos1+1);
+        if (rpos2 == -1)
+        {
+            str1 = rule.substr(rpos1+1, rule.length()-1);
+        }
+        else
+        {
+            str1 = rule.substr(rpos1+1, rpos2-rpos1-1);
+        }
+
+        if (upos2 == -1)
+        {
+            str2 = url.substr(upos1+1, url.length()-1);
+        }
+        else
+        {
+            str2 = url.substr(upos1+1, upos2-upos1-1);
+        }
+    }
     while (flag)
     {
         if (str1 == "<str>")
@@ -56,6 +90,7 @@ int match(string rule, string name, string url)
             {
                 return 0;
             }
+            //match
             str2 = url.substr(upos1+1);
             re = re + " " + str2;
             re = name + re;
@@ -64,48 +99,95 @@ int match(string rule, string name, string url)
         }
         else if (str1 != str2)
         {
-            flag = 0;
+            return 0;
+        }
+        if (rpos2 == -1)
+        {
+            if (upos2 != -1)
+            {
+                return 0;
+            }
         }
         rpos1 = rpos2;
         upos1 = upos2;
-        if (final || rpos1 == (rule.length()-1) || upos1 == (url.length()-1))
-        {
-            break;
-        }
         rpos2 = rule.find('/', rpos1+1);
         upos2 = url.find('/', upos1+1);
-        if (rpos2 == -1)
+        if (rpos1 == -1)
         {
-            str1 = rule.substr(rpos1+1);
-            if (upos2 == -1)
+            if (upos1 != -1)
             {
-                str2 = url.substr(upos1+1);
+                return 0;
             }
             else
             {
-                str2 = url.substr(upos1+1, upos2-upos1-1);
+                //match
+                re = name + re;
+                cout << re << endl;
+                return 1;
             }
-            final = 1;
-            continue;
+        }
+
+        if (rpos2 == -1)
+        {
+            if (rpos1 == rule.length()-1)   //rule has ended
+            {
+                if (upos1 == url.length()-1)
+                {
+                    //match
+                    re = name + re;
+                    cout << re << endl;
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                if (upos1 == url.length()-1)    //url ended
+                {
+                    return 0;
+                }
+                else
+                {
+                    str1 = rule.substr(rpos1+1, rule.length()-1);
+                    if (upos2 == -1)
+                    {
+                        str2 = url.substr(upos1+1, url.length()-1);
+                    }
+                    else
+                    {
+                        str2 = url.substr(upos1+1, upos2-upos1-1);
+                    }
+                    continue;
+                }
+            }
         }
         if (upos2 == -1)
         {
-            str2 = url.substr(upos1+1);
-            final = 1;
-            continue;
+            return 0;
         }
         str1 = rule.substr(rpos1+1, rpos2-rpos1-1);
         str2 = url.substr(upos1+1, upos2-upos1-1);
     }
-    if (rpos1 == rule.length()-1 && upos1 == url.length()-1 && flag)
-    {
-        re = name + re;
-        cout << re << endl;
-        return 1;
-    }
-
 
     return 0;
+}
+
+int is_legal(string url)
+{
+    int i=0, j=url.length();
+    for (i=0; i<j; i++)
+    {
+        char c = url[i];
+        if ((c=='/') || (c>='a'&&c<='z') || (c>='A'&&c<='Z') || (c<='9'&&c>='0') || (c=='-') || (c=='_') || (c=='.'))
+        {
+            continue;
+        }
+        return 0;
+    }
+    return 1;
 }
 
 int main() {
@@ -125,6 +207,11 @@ int main() {
     for (int i=0; i<m; i++)
     {
         find = 0;
+        if (!is_legal(url[i]))
+        {
+            cout << "404" << endl;
+            continue;
+        }
         for (int j=0; j<n; j++)
         {
             if (match(rule[j], name[j], url[i]))
